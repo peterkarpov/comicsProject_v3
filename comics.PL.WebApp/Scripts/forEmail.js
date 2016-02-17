4ac5d4651404adf4c8119e8384f88bb06a86ab51
peter karpov <peter_karpov@mail.ru>
2016-02-17 15:07:32 +0300

    4

--- comics.PL.WebApp/Scripts/forEmail.js
+++ comics.PL.WebApp/Scripts/forEmail.js
@@ -2,7 +2,7 @@
 
     var email = $("#email").val()
 
-    var pattern = /^\w+@@\w+\.\w{2,4}$/i;
+    var pattern = /^\w+@\w+\.\w{2,4}$/i;
     //var pattern = /([A-Za-z0-9]?[._-][A-Za-z0-9]?)+@@([A-Za-z0-9][A-Za-z0-9\-]+[A-Za-z0-9])(\.[A-Za-z]{2,6})+\b/i
 
     if (email.match(pattern) == null) {
